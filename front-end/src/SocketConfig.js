d0f0750209ac0d2ee6efde6454a6fc7d9d0b5eeb
Nathan Hartson <linkhq9@gmail.com>
2026-05-23 17:08:37 -0400

    chessing it

--- front-end/src/SocketConfig.js
+++
@@ -1,6 +0,0 @@
-import openSocket from 'socket.io-client';
-
-const socket = openSocket("https://onlinechess-py-backend.onrender.com", {transports: ['websocket']})
-//const socket = openSocket("http://localhost:8080")
-
-export default socket;
